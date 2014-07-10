7d29f2f654670cc2584610496db3a65146f04231
Ray van B <deslaper@gmail.com>
2014-07-10 18:55:37 +0200

    Finished E05 on larabook

---
+++ larabook.app/app/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+
+@section('content')
+
+	<div class="jumbotron">
+		<h1>Welcome to Larabook!</h1>
+		<p>
+			Welcome to the premier place to talk about Laravel with others. Why don't you sign up and see what all the fuss is about.
+		</p>
+		<p>
+			<a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign up!</a>
+		</p>
+	</div>
+@stop
